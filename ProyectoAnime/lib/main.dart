4ebfb43901f0ca5eda308366a48ffedebea83e55
Adolfo Espinosa <adolfo.espinosa@usm.cl>
2020-11-18 18:40:14 -0300

    Update main.dart

--- ProyectoAnime/lib/main.dart
+++ ProyectoAnime/lib/main.dart
@@ -30,6 +30,47 @@ class MyHomePage extends StatelessWidget {
           height: 50,
         ),
       ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              "Top 10 Mejores",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          DefaultTabController(
+              length: 2,
+              child: TabBar(
+                tabs: [
+                  Tab(
+                    child: Text(
+                      "Anime",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                  Tab(
+                    child: Text(
+                      "Manga",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ],
+              )),
+        ],
+      ),
     );
   }
 }
+
+// soy un cambio by me //
+//Hola mundo world//
